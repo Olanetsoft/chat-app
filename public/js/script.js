570e5304e520360992e6b76c53e64b4ee481c03a
olanetsoft <heedris2olubisi@gmail.com>
2020-09-10 01:29:56 +0100

    CHAT IMPLEMENTED

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,49 @@
-const chatForm = document.getElementById('')
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.getElementById('gbefun');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
 
 const socket = io();
 
+
+// Join Chat Room
+socket.emit('joinRoom', {
+    username, room
+});
+
+// Output message from server
 socket.on('message', message => {
     console.log(message)
-})
+    outputMessage(message);
+
+    // scroll down 
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get chat message
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to the server
+    socket.emit('chatMessage', msg)
+
+    // clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+
+});
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div')
+    div.classList.add('msg_cotainer_send');
+    div.innerHTML = ` ${message.text}
+    <span class="msg_time_send">${message.username} ${message.time}</span>`;
+    document.getElementById('gbefun').appendChild(div);
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages.js');
 
 
 const app = express();
@@ -11,17 +12,30 @@ const io = socketio(server)
 // set static file
 app.use(express.static(path.join(__dirname, 'public')));
 
+// bot name
+const botName = 'Admin BOT'
 
 // Run when a client connect
-io.on('connection', socket =>{
-    socket.emit('message', 'Welcome to live chat');
+io.on('connection', socket => {
 
-    // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user joined the chat');
+    socket.on('joinRoom', ({ username, room }) => {
+        // welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to live chat'));
+
+        // Broadcast when a user connects
+        socket.broadcast.emit('message', formatMessage(botName, 'A user joined the chat'));
+
+    });
+
+    // Listen for chat Message
+    socket.on('chatMessage', msg => {
+        io.emit('message', formatMessage('USER', msg));
+    });
+
 
     // Runs when client disconnect
-    socket.on('disconnect', ()=>{
-        io.emit('message', 'A user had left the chat')
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user had left the chat'));
     });
 });
 
